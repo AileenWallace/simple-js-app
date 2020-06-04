261b6f728691c187bb54566cb5edbe093acb3562
AileenWallace <aliwallac@gmail.com>
2020-06-03 22:23:28 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 alert("Hello");
 var favoriteFood = "avocado";
 document.write(favoriteFood);
+
+var name = "John";
+document.write("My name is " + name + " and I am ready to start!");
+
+var name = "John";
+var age = 42;
+document.write("My name is " + name + " and I'm " + age + " years old!");
