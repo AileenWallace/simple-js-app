cc7099a3f790b1913dd554fb3b955953e740dbcd
AileenWallace <aliwallac@gmail.com>
2020-06-08 19:45:32 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,57 +2,57 @@ var repository = [
   {
     name: "Bulbasaur",
     height: 0.7,
-    types: [" grass", " poison"],
+    types: ["grass", "poison"],
   },
   {
     name: "Ponyta",
     height: 1,
-    types: [" speed", " fire", " agility"],
+    types: ["speed", "fire", "agility"],
   },
   {
     name: "Butterfree",
     height: 1.1,
-    types: [" psychic", " poison"],
+    types: ["psychic", "poison"],
   },
 ];
 
-repository.forEach(function (item) {
+repository.forEach(function (pokemon) {
   var size;
-  if (item.height > 0.8) {
+  if (pokemon.height > 0.8) {
     size = "Wow, that is big!";
   } else {
     size = "It is a small pokemon.";
   }
 
-  var result;
-  item.types.forEach(function (typeItem) {
-    if (typeItem == "grass") {
-      result = '<span style="color:green;"> ';
-    } else if (typeItem == "poison") {
-      result = '<span style="color: #90EE90;"> ';
-    } else if (typeItem == "speed") {
-      result = '<span style="color:purple;"> ';
-    } else if (typeItem == "fire") {
-      result = '<span style="color: #B22222;"> ';
-    } else if (typeItem == "agility") {
-      result = '<span style="color: #FFD700;"> ';
-    } else if (typeItem == "psychic") {
-      result = '<span style="color: #4B0082;"> ';
+  var fontColor;
+  pokemon.types.forEach(function (typepokemon) {
+    if (typepokemon == "grass") {
+      fontColor = '<span style="color: green;"> ';
+    } else if (typepokemon == "poison") {
+      fontColor = '<span style="color: #90EE90;"> ';
+    } else if (typepokemon == "speed") {
+      fontColor = '<span style="color: purple;"> ';
+    } else if (typepokemon == "fire") {
+      fontColor = '<span style="color: #B22222;"> ';
+    } else if (typepokemon == "agility") {
+      fontColor = '<span style="color: #FFD700;"> ';
+    } else if (typepokemon == "psychic") {
+      fontColor = '<span style="color: #4B0082;"> ';
     }
   });
 
   document.write(
     '<div class="box">' +
-      item.name +
+      pokemon.name +
       "(height: " +
-      item.height +
+      pokemon.height +
       "m" +
       ")" +
       "<br>" +
       size +
-      result +
+      fontColor +
       "<br>" +
-      item.types +
+      pokemon.types +
       "<br>" +
       "</div>"
   );
