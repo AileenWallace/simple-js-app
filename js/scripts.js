29e202c4b1286a8b58a6e4df1a52fe46228c7ba3
AileenWallace <aliwallac@gmail.com>
2020-06-15 14:30:25 -0400

    1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,49 @@
-var repository = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Ponyta",
-    height: 1,
-    types: ["speed", "fire", "agility"],
-  },
-  {
-    name: "Butterfree",
-    height: 1.1,
-    types: ["psychic", "poison"],
-  },
-];
+var pokemonRepository = (function () {
+  var pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Ponyta",
+      height: 1,
+      types: ["fire", "agility", "speed"],
+    },
+    {
+      name: "Butterfree",
+      height: 1.1,
+      types: ["poison", "psychic"],
+    },
+  ];
 
-repository.forEach(function (pokemon) {
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("This is not a pokemon");
+    }
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Jigglypuff", height: 0.2, types: ["electric"] });
+pokemonRepository.add({ name: "Gengar", height: 0.2, types: ["water"] });
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
   var size;
   if (pokemon.height > 0.8) {
     size = "Wow, that is big!";
@@ -25,19 +52,23 @@ repository.forEach(function (pokemon) {
   }
 
   var fontColor;
-  pokemon.types.forEach(function (typepokemon) {
-    if (typepokemon == "grass") {
+  pokemon.types.forEach(function (typePokemon) {
+    if (typePokemon == "grass") {
       fontColor = '<span style="color: green;"> ';
-    } else if (typepokemon == "poison") {
-      fontColor = '<span style="color: #90EE90;"> ';
-    } else if (typepokemon == "speed") {
+    } else if (typePokemon == "poison") {
       fontColor = '<span style="color: purple;"> ';
-    } else if (typepokemon == "fire") {
-      fontColor = '<span style="color: #B22222;"> ';
-    } else if (typepokemon == "agility") {
-      fontColor = '<span style="color: #FFD700;"> ';
-    } else if (typepokemon == "psychic") {
-      fontColor = '<span style="color: #4B0082;"> ';
+    } else if (typePokemon == "speed") {
+      fontColor = '<span style="color: darkblue;"> ';
+    } else if (typePokemon == "fire") {
+      fontColor = '<span style="color: red;"> ';
+    } else if (typePokemon == "agility") {
+      fontColor = '<span style="color: orange;"> ';
+    } else if (typePokemon == "psychic") {
+      fontColor = '<span style="color: brown;"> ';
+    } else if (typePokemon == "electric") {
+      fontColor = '<span style="color: yellow;"> ';
+    } else if (typePokemon == "water") {
+      fontColor = '<span style="color: blue;"> ';
     }
   });
 
@@ -56,5 +87,5 @@ repository.forEach(function (pokemon) {
       "<br>" +
       "</div>"
   );
+  console.log(pokemonRepository);
 });
-console.log(repository);
