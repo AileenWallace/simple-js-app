9f8275a405f27dd019b16ede7fa7b4eb7d12d25b
AileenWallace <aliwallac@gmail.com>
2020-07-01 13:04:04 -0400

    1.7

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
 var pokemonRepository = (function () {
-  var pokemonList = [
-    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-    { name: "Ponyta", height: 1, types: ["fire", "agility", "speed"] },
-    { name: "Butterfree", height: 1.1, types: ["poison", "psychic"] },
-  ];
+  var pokemonList = [];
+  var apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
       "name" in pokemon &&
-      "height" in pokemon &&
-      "types" in pokemon
+      "detailsUrl" in pokemon
     ) {
       pokemonList.push(pokemon);
     } else {
@@ -32,48 +28,57 @@ var pokemonRepository = (function () {
       showDetails(pokemon);
     });
   }
-  function showDetails(pokemon) {
-    console.log(pokemon);
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          var pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+          console.log(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  function loadDetails(item) {
+    var url = item.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
   }
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
   };
 })();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: "Jigglypuff", height: 0.2, types: ["electric"] });
-pokemonRepository.add({ name: "Gengar", height: 0.2, types: ["water"] });
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  var size;
-  if (pokemon.height > 0.8) {
-    size = "Wow, that is big!";
-  } else {
-    size = "It is a small pokemon.";
-  }
-
-  var fontColor;
-  pokemon.types.forEach(function (typePokemon) {
-    if (typePokemon == "grass") {
-      fontColor = '<span style="color: green;"> ';
-    } else if (typePokemon == "poison") {
-      fontColor = '<span style="color: purple;"> ';
-    } else if (typePokemon == "speed") {
-      fontColor = '<span style="color: darkblue;"> ';
-    } else if (typePokemon == "fire") {
-      fontColor = '<span style="color: red;"> ';
-    } else if (typePokemon == "agility") {
-      fontColor = '<span style="color: orange;"> ';
-    } else if (typePokemon == "psychic") {
-      fontColor = '<span style="color: brown;"> ';
-    } else if (typePokemon == "electric") {
-      fontColor = '<span style="color: yellow;"> ';
-    } else if (typePokemon == "water") {
-      fontColor = '<span style="color: blue;"> ';
-    }
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
   });
-  pokemonRepository.addListItem(pokemon);
 });
